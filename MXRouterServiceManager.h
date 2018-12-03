90b994ff21906eb42e96e4f2cb4a1df233491f07
chenxueming <1391801900@qq.com>
2018-12-03 17:45:06 +0800

    更新代码

---
+++ MXRouterServiceManager.h
@@ -0,0 +1,16 @@
+//
+//  MXRouterServiceManager.h
+//  FBSnapshotTestCase
+//
+//  Created by muxue on 2018/12/3.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MXRouterServiceManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MXRouterServiceManager.m
@@ -0,0 +1,12 @@
+//
+//  MXRouterServiceManager.m
+//  FBSnapshotTestCase
+//
+//  Created by muxue on 2018/12/3.
+//
+
+#import "MXRouterServiceManager.h"
+
+@implementation MXRouterServiceManager
+
+@end
